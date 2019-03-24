b657246645a0cc6dd60cee0a15b73256b2e3fb4c
Mark Murphy <markpub@flnet.net>
2019-03-24 12:55:33 -0400

    Communicating with a Child Component (completed)

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -3,21 +3,31 @@ import { Component, OnInit, ViewChild, AfterViewInit, ElementRef, QueryList, Vie
 import { IProduct } from './product';
 import { ProductService } from './product.service';
 import { NgModel } from '@angular/forms';
+import { CriteriaComponent } from '../shared/criteria/criteria.component';
 
 @Component({
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent implements OnInit, AfterViewInit {
+
+    // ViewChild establishes a reference to a child component
+    // This reference can be based on a hashtag added to the parent template (as shown)
+    // or a child component type if only one child component of that type is used in
+    // the parent template (code commented out below)
+    @ViewChild('filterCriteria') filterElementRef: CriteriaComponent;
+    // @ViewChild(CriteriaComponent) filterElementRef: CriteriaComponent;
+
     pageTitle: string = 'Product List';
     showImage: boolean;
     imageWidth: number = 50;
     imageMargin: number = 2;
     errorMessage: string;
+    parentFilter: string;
 
     filteredProducts: IProduct[];
     products: IProduct[];
-    includeFilterDetail: boolean = false;
+    includeFilterDetail: boolean = true;
 
     constructor(private productService: ProductService) { }
 
@@ -25,12 +35,15 @@ export class ProductListComponent implements OnInit {
         this.productService.getProducts().subscribe(
             (products: IProduct[]) => {
                 this.products = products;
-                this.performFilter();
+                this.performFilter(this.parentFilter);
             },
             (error: any) => this.errorMessage = <any>error
         );
     }
 
+    ngAfterViewInit(): void {
+        this.parentFilter = this.filterElementRef.listFilter;
+    }
     toggleImage(): void {
         this.showImage = !this.showImage;
     }
--- APM-Start/src/app/shared/criteria/criteria.component.ts
+++ APM-Start/src/app/shared/criteria/criteria.component.ts
@@ -1,20 +1,43 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Input } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Input, OnChanges, SimpleChange, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'pm-criteria',
   templateUrl: './criteria.component.html',
   styleUrls: ['./criteria.component.css']
 })
-export class CriteriaComponent implements OnInit, AfterViewInit {
+export class CriteriaComponent implements OnInit, AfterViewInit, OnChanges {
 
   listFilter: string;
 
   @ViewChild('filterElement') filterElementRef: ElementRef;
   @Input() includeDetail: boolean;
+  @Input() hitCount: number;
+  hitMessage: string;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    if (changes['hitCount']) {
+      switch (this.hitCount)
+      {
+        case 0:
+          this.hitMessage = 'No matches found';
+          break;
+        case 1:
+          this.hitMessage = '1 match found';
+          break;
+        default:
+          this.hitMessage = `${this.hitCount} matches found`;
+          break;
+      }
+      if (this.hitCount === 0) {
+      } else {
+      }
+    }
   }
 
   ngAfterViewInit(): void {
